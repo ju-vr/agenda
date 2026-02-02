d65cc45877c9049343ffcfdd2b2a3474a3afaa04
ju-vr <juliana.ruzisca@aluno.senai.br>
2026-02-02 16:45:36 -0300

    feat: salvando as tarefas no banco SQlite3

--- teste.py
+++ teste.py
@@ -1,1 +1,13 @@
-print("ola, Python")
+from models.database import Database
+from typing import Self
+
+class Tarefa: 
+    def __init__(self: Self, titulo_tarefa: str, data_conclusao: str = None) -> None:
+        self.titulo_tarefa: str = titulo_tarefa
+        self.data_conclusao: str = data_conclusao
+
+    def salvar_tarefa(self: Self) -> None:
+        with Database('.data/tarefas.sqlite3') as db:
+            query: str = "INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
+            params: tuple = (self.titulo_tarefa, self.data_conclusao)
+            db.executar(query, params)
