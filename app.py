5f4fefef5082dd752166c9209c454e8974c18ae8
ju-vr <juliana.ruzisca@aluno.senai.br>
2026-02-10 13:28:19 -0300

    atualição da agenda

--- app.py
+++ app.py
@@ -1,47 +1,50 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from models.tarefa import Tarefa
+from models.database import init_db
 
 app = Flask(__name__)
+
+init_db()
 
 @app.route('/')
 def home():
     return render_template('home.html', titulo='Home')
-
-
-@app.route('/ola')
-def ola_mundo():
-    return "olá, mundo"
 
 @app.route('/agenda', methods=['GET', 'POST'])
 def agenda():
     tarefas = None
     
     if request.method == 'POST':
-        titulo_tarefa = request.form ['titulo-tarefa']
+        titulo_tarefa = request.form['titulo-tarefa']
         data_conclusao = request.form['data-conclusao']
-        tarefa = Tarefa(titulo_tarefa, data_conclusao) 
+        tarefa = Tarefa(titulo_tarefa, data_conclusao)
         tarefa.salvar_tarefa()
 
     tarefas = Tarefa.obter_tarefas()
-    return render_template('agenda.html', titulo= 'Agenda', tarefas=tarefas)
+    return render_template('agenda.html', titulo='Nova tarefa', tarefas=tarefas)
 
 @app.route('/delete/<int:idTarefa>')
 def delete(idTarefa):
     tarefa = Tarefa.id(idTarefa)
     tarefa.excluir_tarefa()
+    # return render_template('agenda.html', titulo="Agenda", tarefas=tarefas)
     return redirect(url_for('agenda'))
 
-@app.route('/update/<int:idTarefa>' , methods=['GET', 'POST'])
+@app.route('/update/<int:idTarefa>', methods = ['GET', 'POST'])
 def update(idTarefa):
-
-    if request.method == 'POST':
-        titulo = request.form ['titulo-tarefa']
-        data = request.form['data-conclusao']
-        tarefa = Tarefa(titulo, data, idTarefa) 
-        tarefa.atualizar_tarefa()
-        return redirect(url_for('agenda'))
     
-    tarefas = Tarefa.obter_tarefas()
-    tarefa_selecionada = Tarefa.id(idTarefa)
+    if request.method == 'POST':
+        titulo = request.form['titulo-tarefa']
+        data = request.form['data-conclusao']
+        tarefa = Tarefa(titulo, data, idTarefa)
+        tarefa.atualizar_tarefa()
+        return redirect(url_for('agenda')) # early return
 
-    return render_template('agenda.html', titulo= 'Agenda', tarefas=tarefas, tarefa_selecionada=tarefa_selecionada)
+    tarefas = Tarefa.obter_tarefas()
+    tarefa_selecionada = Tarefa.id(idTarefa) # seleção da tarefa que será editada
+
+    return render_template('agenda.html', titulo=f'Editando a tarefa ID: {idTarefa}', tarefas=tarefas, tarefa_selecionada=tarefa_selecionada)
+
+@app.route('/ola')
+def ola_mundo():
+    return "Olá, Mundo!"
--- models/database.py
+++ models/database.py
@@ -1,8 +1,29 @@
 from sqlite3 import Connection, connect, Cursor
-from typing import Any
+from types import TracebackType
+from typing import Any, Self, Optional, Type
+from dotenv import load_dotenv
+import traceback
+import os
+
+load_dotenv
+DB_PATH = os.getenv('DATABASE', './data/tarefas.sqlite3')
+
+def init_db(db_name: str = DB_PATH) -> None:
+    with connect(db_name) as conn:
+        conn.execute("""
+        CREATE TABLE IF NOT EXISTS tarefas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo_tarefa TEXT NOT NULL,
+            data_conclusao TEXT             
+        );""")
 
 class Database:
-    def __init__(self, db_name: str) -> None:
+    """
+        Classe que gerencia conexoes e operações com um banco de dados
+        SQLite. Utiliza o protocolo de gerenciamento de contexto para garantir que 
+        a conexao seja encerrada corretamente.
+    """
+    def __init__(self, db_name: str = DB_PATH) -> None:
         self.connection: Connection = connect(db_name)
         self.cursor: Cursor = self.connection.cursor()
 
@@ -18,28 +39,24 @@ class Database:
     def close(self) -> None:
         self.connection.close()
 
-    # Métodos para o gerenciamento de contexto
-
-    # Método de entrada no contexto
-    def __enter__(self):
-        print('Entrando no contexto...')
+    
+    
+    def __enter__(self) -> Self:
         return self
     
     # Método de saída do contexto
-    def __exit__(self, exc_type, exc_value, traceback):
-        print('Saindo no contexto...')
+    def __exit__(
+            self, 
+            exc_type: Optional[Type[BaseException]], 
+            exc_value: Optional[BaseException], 
+            tb: Optional[TracebackType]) -> None:
+        
+        if exc_type is not None:
+            print('Exceção capturada no contexto:')
+            print(f'Tipo: {exc_type.__name__}')
+            print(f'Mensagem: {exc_value}')
+            print('Traceback completo:')
+            traceback.print_tb(tb)
+
         self.close()
 
-# try:
-#     db = Database('./data/tarefas.sqlite3')
-#     db.executar('''
-#     CREATE TABLE IF NOT EXISTS tarefas (
-#         id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         titulo_tarefa TEXT NOT NULL,
-#         data_conclusao TEXT);
-#     ''')
-#     db.executar('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);', ('Estudar Python', '2026-02-02'))
-# except Exception as e:
-#     print(f"Erro ao criar a tabela: {e}")
-# finally:
-#     db.close()
